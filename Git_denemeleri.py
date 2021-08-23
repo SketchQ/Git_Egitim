a60581de1b60cdaf9f5f1013b36c4bdb86bc6bc8
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-23 21:20:40 +0300

    git_denemeleri yeni branchde açıldı

---
+++ Git_denemeleri.py
@@ -0,0 +1,8 @@
+# bu klasörde git eğitimi için basit denemeler yapılacaktır.
+
+## dikkate almayın.
+
+## Bu satırlar yeni bir branch içerisinde deneme amaçlı açılmıştır
+
+## Branch adı : gitdeneme
+
