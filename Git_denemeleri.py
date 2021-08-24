29a67f105d6ffbe3730aa8193d6b08475daf08eb
Erim Serdönmez <eserdnmez@gmail.com>
2021-08-24 22:02:52 +0300

    Git Notları changed to .md

--- Git_denemeleri.py
+++
@@ -1,7 +0,0 @@
-# bu klasörde git eğitimi için basit denemeler yapılacaktır.
-
-## dikkate almayın.
-
-## Bu satırlar yeni bir branch içerisinde deneme amaçlı açılmıştır
-
-## Branch adı : gitdeneme
